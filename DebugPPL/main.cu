a0757d13bafb129ab61f43bc163d498b42fbe4ab
Ivan Xu <info@chivalrystudio.com>
2024-03-24 23:12:42 -0700

    everything looks fine

--- DebugPPL/main.cu
+++ DebugPPL/main.cu
@@ -12,6 +12,8 @@
 
 #include <execution> // just for speeding up the test
 
+#include "shared/edge_func.h"
+
 // ------------------ configs ------------------
 //constexpr auto n = 1920 * 1080; // ~2M
 
@@ -19,6 +21,10 @@ constexpr auto n = 640 * 480; // ~300k
 constexpr auto min_coord = 0.0f;
 constexpr auto range = 1024.0f;
 constexpr auto seed = 114514;
+
+// used for quickly setting up the test
+constexpr auto test_case_grid_size = 16;
+
 
 [[nodiscard]] std::unique_ptr<pipe> generate_pipe()
 {
@@ -84,8 +90,8 @@ void test_unique(const int grid_size)
 
 	// generate GPU result
 	constexpr auto stream_id = 0;
-	gpu::dispatch_ComputeMorton(grid_size, stream_id, *gpu_pip);
-	gpu::dispatch_RadixSort(grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_ComputeMorton(test_case_grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_RadixSort(test_case_grid_size, stream_id, *gpu_pip);
 	SYNC_DEVICE();
 
 	// generate CPU result (assume previous test is correct)
@@ -103,7 +109,6 @@ void test_unique(const int grid_size)
 
 	EXPECT_EQ(cpu_n_unique, gpu_n_unique);
 
-	// the data after 'n_unique' is 0 in GPU version
 	for (auto i = 0; i < gpu_n_unique; i++)
 	{
 		EXPECT_EQ(cpu_morton_alt[i], gpu_pip->u_morton_alt[i]);
@@ -128,36 +133,34 @@ void test_binary_radix_tree(const int grid_size)
 
 	// generate GPU result
 	constexpr auto stream_id = 0;
-	gpu::dispatch_ComputeMorton(grid_size, stream_id, *gpu_pip);
-	gpu::dispatch_RadixSort(grid_size, stream_id, *gpu_pip);
-	gpu::dispatch_RemoveDuplicates_sync(grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_ComputeMorton(test_case_grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_RadixSort(test_case_grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_RemoveDuplicates_sync(test_case_grid_size, stream_id, *gpu_pip);
 	SYNC_DEVICE();
 
 	const std::vector cpu_morton(gpu_pip->u_morton_alt, gpu_pip->u_morton_alt + n);
 
-	// Yanwen: without this -1 one we will get errors.
-	const auto cpu_n_unique = gpu_pip->n_unique_mortons() - 1;
+	const auto cpu_n_brt_nodes = gpu_pip->n_brt_nodes();
 
 	// ------- testing region ------------
 	gpu::dispatch_BuildRadixTree(grid_size, stream_id, *gpu_pip);
 	gpu::sync_stream(stream_id);
 	// -----------------------------------
 
-	radix_tree cpu_tree(cpu_n_unique);
+	const radix_tree cpu_tree(cpu_n_brt_nodes);
 
-	for (auto i = 0; i < cpu_n_unique; i++)
+	for (auto i = 0; i < cpu_n_brt_nodes; i++)
 	{
-		cpu::process_radix_tree_i(i, cpu_n_unique, cpu_morton.data(), &cpu_tree);
+		cpu::process_radix_tree_i(i, cpu_n_brt_nodes, cpu_morton.data(), &cpu_tree);
 	}
 
-	for (auto i = 0; i < cpu_n_unique; i++)
+	for (auto i = 0; i < cpu_n_brt_nodes; i++)
 	{
 		ASSERT_EQ(cpu_tree.u_prefix_n[i], gpu_pip->brt.u_prefix_n[i])
               << "Mismatch at index " << i;
 
 		ASSERT_EQ(cpu_tree.u_has_leaf_left[i], gpu_pip->brt.u_has_leaf_left[i]);
 		ASSERT_EQ(cpu_tree.u_has_leaf_right[i], gpu_pip->brt.u_has_leaf_right[i]);
-
 		ASSERT_EQ(cpu_tree.u_left_child[i], gpu_pip->brt.u_left_child[i]);
 		ASSERT_EQ(cpu_tree.u_parent[i], gpu_pip->brt.u_parent[i]);
 	}
@@ -168,6 +171,55 @@ TEST(BinaryRadixTree, GridSize)
 	for (auto i = 1; i < 16; i++)
 	{
 		EXPECT_NO_FATAL_FAILURE(test_binary_radix_tree(i));
+	}
+}
+
+// ===============================================
+//	Edge Count + Offset
+// ===============================================
+
+void test_edge_count(const int grid_size)
+{
+	auto gpu_pip = generate_pipe();
+
+	// generate GPU result
+	constexpr auto stream_id = 0;
+	gpu::dispatch_ComputeMorton(test_case_grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_RadixSort(test_case_grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_RemoveDuplicates_sync(test_case_grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_BuildRadixTree(test_case_grid_size, stream_id, *gpu_pip);
+	SYNC_DEVICE();
+
+
+	// ------- testing region ------------
+	gpu::dispatch_EdgeCount(grid_size, stream_id, *gpu_pip);
+	gpu::dispatch_EdgeOffset(grid_size, stream_id, *gpu_pip);
+	gpu::sync_stream(stream_id);
+	// -----------------------------------
+
+	const auto cpu_n_brt_nodes = gpu_pip->n_brt_nodes();
+	std::vector<int> cpu_edge_count(cpu_n_brt_nodes);
+	std::vector<int> cpu_edge_offset(cpu_n_brt_nodes);
+
+	for (auto i = 0; i < cpu_n_brt_nodes; i++)
+	{
+		shared::process_edge_count_i(i, gpu_pip->brt.u_prefix_n, gpu_pip->brt.u_parent, cpu_edge_count.data());
+	}
+	std::exclusive_scan(cpu_edge_count.begin(), cpu_edge_count.end(), cpu_edge_offset.begin(), 0);
+
+	for (auto i = 0; i < cpu_n_brt_nodes; i++)
+	{
+		ASSERT_EQ(cpu_edge_count[i], gpu_pip->u_edge_count[i]) << "Mismatch at index " << i;
+		ASSERT_EQ(cpu_edge_offset[i], gpu_pip->u_edge_offset[i]) << "Mismatch at index " << i;
+	}
+}
+
+
+TEST(TestEdgeCount, GridSize)
+{
+	for (auto i = 1; i < 16; i++)
+	{
+		EXPECT_NO_FATAL_FAILURE(test_edge_count(i));
 	}
 }
 
